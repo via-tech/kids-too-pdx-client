7499539610d0ec78ad017841b6e289e8ae37e2f5
Dyanna Tolman <dyannast@gmail.com>
2019-03-24 02:04:04 -0700

    scaffold and basic files

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom';
+// import { getRoutes } from '../../routes';
+// import Header from '../header/Header';
+
+export default function App() {
+  return (
+      <>
+       <h1>Hello</h1>
+      </>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const DotenvPlugin = require('dotenv-webpack');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.[hash].js',
+    publicPath: '/'
+  },
+  devServer: {
+    port: 7890,
+    historyApiFallback: true
+  },
+  plugins: [
+    new DotenvPlugin({ systemvars: true }),
+    new HtmlPlugin({ template: './src/index.html' }),
+    new CleanPlugin('./dist')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                require('autoprefixer')(),
+                require('postcss-nested')()
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpeg|jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 },
+        },
+      }
+    ]
+  }
+};
