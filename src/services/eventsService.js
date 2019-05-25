856cd3c57c64c5ccd38b8bdedf562f1b03edebef
Shaba9 <shaba92@gmail.com>
2019-05-24 18:33:17 -0700

    events services tests pass

--- src/services/eventsService.js
+++ src/services/eventsService.js
@@ -5,11 +5,14 @@ import {
   patch
 } from './request';
 
-export const postEvent = event => post('/events', event);
+export const postEvent = event => post('/events', event)
+  .catch(err => err);
 
-export const getEvents = () => get('/events');
+export const getEvents = () => get('/events')
+  .catch(err => err);
 
-export const getEvent = id => get(`/events/${id}`);
+export const getEvent = id => get(`/events/${id}`)
+  .catch(err => err);
 
 export const getFilteredEvents = filters => {
   let route = '/events/query/q?';
@@ -18,19 +21,31 @@ export const getFilteredEvents = filters => {
     return filters[key] ? route += `${key}=${filters[key]}&` : route;
   });
 
-  return get(route);
+  return get(route)
+    .catch(err => err);
 };
 
-export const patchEvent = id => patch(`/events/${id}`);
+export const patchEvent = updatedEvent =>
+  patch(`/events/${updatedEvent._id}`, updatedEvent)
+    .catch(err => err);
 
-export const deleteEvent = event => del(`/events/${event._id}`, event);
+export const deleteEvent = event =>
+  del(`/events/${event._id}`, event)
+    .catch(err => err);
 
-export const signUp = user => post('/auth/signup', user);
+export const signUp = user => post('/auth/signup', user)
+  .catch(err => err);
 
-export const signIn = user => post('/auth/signin', user);
+export const signIn = user => post('/auth/signin', user)
+  .catch(err => err);
 
-export const patchUser = (id, user) => patch(`/auth/${id}`, user);
+export const patchUser = (id, user) =>
+  patch(`/auth/${id}`, user)
+    .catch(err => err);
 
-export const getOrgs = () => get('/orgs');
+export const getOrgs = () => get('/orgs')
+  .catch(err => err);
 
-export const deleteOrg = org => del(`/orgs/${org.user._id}`, org);
+export const deleteOrg = org =>
+  del(`/orgs/${org.user._id}`, org)
+    .catch(err => err);
--- src/services/eventsService.test.js
+++ src/services/eventsService.test.js
@@ -2,6 +2,9 @@ import { seedTestData, deleteTestData } from './seedTestData';
 
 import {
   getEvents,
+  getEvent,
+  getFilteredEvents,
+  patchEvent,
   signIn
 } from './eventsService';
 
@@ -22,10 +25,66 @@ describe('request', () => {
 
   afterAll(() => deleteTestData());
 
+  it('posts events', () => expect(events).toHaveLength(5));
+
   it('gets events', () =>
     getEvents()
       .then(events => expect(events).toBeDefined())
   );
+
+  it('gets an event by id', () => {
+    getEvent(events[0]._id)
+      .then(event => expect(event).toEqual({
+        _id: events[0]._id,
+        user: user.user._id,
+        image: expect.any(String),
+        name: 'The Event 1',
+        ageMin: 8,
+        ageMax: 12,
+        category: 'Art',
+        liability: true
+      }));
+  });
+
+  it('gets filtered event', () => {
+    const filters = {
+      category: 'Education',
+      ageMin: 14,
+      ageMax: 25
+    };
+
+    getFilteredEvents(filters)
+      .then(filteredEvents => expect(filteredEvents).toEqual([{
+        _id: events[3]._id,
+        user: user.user._id,
+        image: expect.any(String),
+        name: 'The Event 4',
+        ageMin: 15,
+        ageMax: 20,
+        category: 'Education',
+        liability: true
+      }]));
+  });
+
+  it('patches an event', () => {
+    const updatedEvent = {
+      _id: events[4]._id,
+      token: user.token,
+      category: 'Music'
+    };
+
+    patchEvent(updatedEvent)
+      .then(patchedEvent => expect(patchedEvent).toEqual({
+        image: expect.any(String),
+        _id: events[4]._id,
+        name: 'The Event 5',
+        ageMin: 8,
+        ageMax: 12,
+        category: 'Music',
+        liability: true,
+        user: user.user._id
+      }));
+  });
 
   it('signs up an organization', () => {
     expect(user).toEqual({
@@ -54,6 +113,4 @@ describe('request', () => {
         token: expect.any(String)
       }))
   );
-
-  it('posts events', () => expect(events).toHaveLength(5));
 });
