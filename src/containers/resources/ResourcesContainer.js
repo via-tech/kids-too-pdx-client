8b36ce7dc8e57dc27dd6e735e5a40d272e2d66ed
Shaba9 <shaba92@gmail.com>
2019-05-26 11:33:43 -0700

    ResourcesContainer added

---
+++ src/containers/resources/ResourcesContainer.js
@@ -0,0 +1,20 @@
+import Resources from '../../components/resources/Resources';
+import { connect } from 'react-redux';
+import { getOrgs } from '../../selectors/resources';
+import { fetchOrgs } from '../../actions/resources';
+import { withFetch } from '../../components/withFetch';
+
+const mapStateToProps = state => ({
+  orgs: getOrgs(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    return dispatch(fetchOrgs());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withFetch(Resources));
